3cf91ed4b388cfe1f072c6e2ae87375c5996759d
Dat Hoang <htdat@users.noreply.github.com>
2020-04-02 20:37:48 +0700

    Update convertCurrency() #3
    Move it to a separate file/functions, and handle when baseCurrency is 
    not USD

--- src/components/To.js
+++ src/components/To.js
@@ -2,6 +2,21 @@ import React from 'react';
 import Currency from './Currency/Currency.js';
 import rates from '../states/rates.js';
 
+function convert(baseCurrency, changeCurrency, baseAmount) {
+
+  // All rates are from USD to other currencies
+  const usdToBase = parseFloat(rates.rates[baseCurrency] ); // E.g: USD/EUR = 0.9
+  const usdToChange = parseFloat( rates.rates[changeCurrency] ); // E.g: USD/CNY = 7.1
+  const baseToChange = usdToChange / usdToBase // E.g: EUR/CNY = (USD/CNY) / (USD/EUR)
+
+  const convertedAmount = baseToChange * parseFloat(baseAmount);
+
+  if (isNaN(convertedAmount)) return null;
+
+  return convertedAmount.toLocaleString('fullwide', {
+    maximumFractionDigits: 2
+  })
+}
 
 class To extends React.Component {
 
@@ -9,19 +24,9 @@ class To extends React.Component {
     super(props);
   }
 
-  convertCurrency(baseCurrency, changeCurrency, baseAmount) {
-    const convertedAmount = parseFloat(rates.rates[changeCurrency]) * parseFloat(baseAmount);
-
-    if (isNaN(convertedAmount)) return null;
-
-    return convertedAmount.toLocaleString('fullwide', {
-      maximumFractionDigits: 2
-    })
-  }
-
   toBlock(currencyCode) {
     const {baseCurrency, baseAmount} = this.props.data;
-    const convertedAmount = this.convertCurrency(baseCurrency, currencyCode, baseAmount);
+    const convertedAmount = convert(baseCurrency, currencyCode, baseAmount);
     return (
       <figure className="change-currency" key={currencyCode}>
         <button
