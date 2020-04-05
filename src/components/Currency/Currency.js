57e6f5072efc5b3a08ae77acd25fdc9fcec7e1b1
Dat Hoang <htdat@users.noreply.github.com>
2020-04-05 15:30:17 +0700

    Small naming update for Currenciy block

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import 'currency-flags/dist/currency-flags.css'
 import './Currency.css'
-import currencies from '../../const/currencies.json';
+import currencyNames from '../../const/currencies.json';
 
 class Currency extends React.Component {
 
   render() {
     const code = this.props.code.toUpperCase();
     if (typeof(code) === 'undefined') return null;
-    if (! currencies.hasOwnProperty(code)) return null;
+    if (! currencyNames.hasOwnProperty(code)) return null;
 
     return (
       <div className="currency">
         <div className={"currency-flag currency-flag-lg currency-flag-" + code.toLowerCase()}></div>
         <div className="code">{code}</div>
-        <div className="name">{currencies[code]}</div>
+        <div className="name">{currencyNames[code]}</div>
       </div>
     );
   }
