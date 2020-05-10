fd14041883828a4870dcfd0ce6b33e6da3592a3d
Dat Hoang <htdat@users.noreply.github.com>
2020-05-10 16:12:18 +0700

    Add the formatted baseAmount into From component

--- src/components/From.js
+++ src/components/From.js
@@ -13,12 +13,19 @@ class From extends React.Component {
   }
 
   render() {
+    const {baseCurrency, baseAmount} = this.props;
+    const formattedBaseAmount = isNaN(parseFloat(baseAmount))
+      ? ''
+      : parseFloat(baseAmount).toLocaleString('fullwide', { maximumFractionDigits: 2})
+
     return (
       <div className="From">
       <h2>From:</h2>
         <figure className="from-currency">
-          <Currency code={this.props.baseCurrency}/>
-          <input value={this.props.baseAmount} onChange={this.changeAmount} type="number"/>
+          <Currency code={baseCurrency}/>
+          <input value={baseAmount} onChange={this.changeAmount} type="number"/>
+          <div className="amount"><span>{baseCurrency}</span> {formattedBaseAmount}</div>
+
         </figure>
       </div>
     );
