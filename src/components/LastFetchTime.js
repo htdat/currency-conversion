a662a2aecaa31749e5072f9b9207b7c3dde3be2b
Dat Hoang <htdat@users.noreply.github.com>
2020-05-17 22:12:28 +0700

    Add proptypes to all components (#9)
    * Add prop-types to the repo
    
    * Add prop-types for EditCurrencies component
    
    * Add prop-types for Settings component
    
    * Add prop-types for From component
    
    * Add prop-types for To component
    
    * Add prop-types for LastFetchTime component

--- src/components/LastFetchTime.js
+++ src/components/LastFetchTime.js
@@ -1,12 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default class LastFetchTime extends React.Component {
-  render() {
-    return this.props.time ? (
-      <div>
-        Exchange rates - last update:{" "}
-        <span>{new Date(this.props.time).toString()}</span>
-      </div>
-    ) : null;
-  }
+export default function LastFetchTime({ timestamp }) {
+  console.log(timestamp);
+  return timestamp ? (
+    <div>
+      Exchange rates - last update:{" "}
+      <span>{new Date(timestamp).toString()}</span>
+    </div>
+  ) : null;
 }
+
+LastFetchTime.propTypes = {
+  timestamp: PropTypes.number,
+};
