88a3f0a2335ab9b204f3b6a0629306a5b6cd02ca
Dat Hoang <htdat@users.noreply.github.com>
2020-07-06 20:56:22 +0700

    Small improvement for LastFetchTime

--- src/components/LastFetchTime.js
+++ src/components/LastFetchTime.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function LastFetchTime({ timestamp }) {
-  console.log(timestamp);
-  return timestamp ? (
-    <div>
-      Exchange rates - last update:{" "}
-      <span>{new Date(timestamp).toString()}</span>
-    </div>
-  ) : null;
+  return (
+    timestamp && (
+      <div>
+        Exchange rates - last update:{" "}
+        <span>{new Date(timestamp).toString()}</span>
+      </div>
+    )
+  );
 }
 
 LastFetchTime.propTypes = {
